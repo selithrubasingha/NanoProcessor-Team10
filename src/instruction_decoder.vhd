1375f6631e1d56a37ae581c31821d6fbc216ad79
Selith <selithrubasingha@gmail.com>
2026-04-28 15:21:49 +0530

    feat : input and outputs of instruction decoder

---
+++ src/instruction_decoder.vhd
@@ -0,0 +1,24 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity instruction_decoder is
+    Port (
+        Instruction : in  STD_LOGIC_VECTOR(11 downto 0);
+        Zero        : in  STD_LOGIC;   -- ALU Zero flag; used for JZR jump decision
+        RegSel      : out STD_LOGIC_VECTOR(2 downto 0);
+        RegEn       : out STD_LOGIC;
+        MuxA_Sel    : out STD_LOGIC_VECTOR(2 downto 0);
+        MuxB_Sel    : out STD_LOGIC_VECTOR(2 downto 0);
+        AddSub      : out STD_LOGIC;
+        ImmVal      : out STD_LOGIC_VECTOR(3 downto 0);
+        ImmMuxSel   : out STD_LOGIC;
+        JumpFlag    : out STD_LOGIC;
+        JumpAddr    : out STD_LOGIC_VECTOR(2 downto 0)
+    );
+end instruction_decoder;
+
+architecture Behavioral of instruction_decoder is
+
+
+end Behavioral;
