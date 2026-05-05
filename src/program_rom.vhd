a8ba72c7b5bd547fb78c67efb1f276b43a9ac936
anuhasranasinghe <anuhasranasinghe321a4@gmail.com>
2026-05-05 09:55:12 +0530

    feat: Updated the program ROM

--- src/program_rom.vhd
+++ src/program_rom.vhd
@@ -13,30 +13,14 @@ begin
     process(Addr)
     begin
         case Addr is
-            -- Line 0: MOVI R1, 1
-            when "000" => Instruction <= "100010000001";
-
-            -- Line 1: MOVI R2, 2
-            when "001" => Instruction <= "100100000010";
-
-            -- Line 2: MOVI R3, 3
-            when "010" => Instruction <= "100110000011";
-
-            -- Line 3: ADD R1, R2  -> R1 = 1+2 = 3
-            when "011" => Instruction <= "000010100000";
-
-            -- Line 4: ADD R1, R3  -> R1 = 3+3 = 6
-            when "100" => Instruction <= "000010110000";
-
-            -- Line 5: ADD R7, R1  -> R7 = 0+6 = 6  (store result)
-            when "101" => Instruction <= "001110010000";
-
-            -- Line 6: JZR R0, 6  -> R0=0, always jump to address 6 (halt)
-            when "110" => Instruction <= "110000000110";
-
-            -- Line 7: Safety padding
-            when "111" => Instruction <= "110000000110";
-
+            when "000" => Instruction <= "100010000101"; -- MOVI R1, 5
+            when "001" => Instruction <= "100100000011"; -- MOVI R2, 3
+            when "010" => Instruction <= "100011000000"; -- PUSH R1 (Stack: [5])
+            when "011" => Instruction <= "100101000000"; -- PUSH R2 (Stack: [5, 3])
+            when "100" => Instruction <= "100110000000"; -- MOVI R3, 0 (Clear R3)
+            when "101" => Instruction <= "100111010000"; -- POP R3  (R3 should be 3)
+            when "110" => Instruction <= "100111010000"; -- POP R3  (R3 should be 5)
+            when "111" => Instruction <= "110000000111"; -- JZR R0, 7 (Halt)
             when others => Instruction <= (others => '0');
         end case;
     end process;
